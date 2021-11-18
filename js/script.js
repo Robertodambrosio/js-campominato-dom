1b414f0b7c1350e5681b0ab7f2bf9998147631fa
Roberto D'Ambrosio <roberto8601@gmail.com>
2021-11-18 18:53:18 +0100

    decorazioni a caso

--- js/script.js
+++ js/script.js
@@ -6,6 +6,9 @@ const container = document.querySelector('.container');
 
 // ======================== Funzioni
 
+
+
+// ************funzione che switcha la difficoltà in base alla scelta dell'utente
 function playFunction() {
     container.innerHTML = "";
     const levelSelected = document.getElementById('gameLevel').value;
@@ -34,22 +37,14 @@ function playFunction() {
     }
 }
 
-
-
-
-
-
-
-
-
 // ************funzione per aggiungere i quadrati dentro il container
 function addSquare(num) {
     for (let i = 1; i <= num; i++) {
         const square = document.createElement('div');
         square.className = 'square';
-        square.textContent = i;
         container.appendChild(square);
         square.addEventListener('click', function () {
+            square.innerHTML = `<i class="fas fa-tint"></i>`; 
             square.classList.add('correct');
         });
     }
@@ -69,23 +64,19 @@ function bombGenerator(num) {
 }
 
 
-// ****************funzione per inserire le bombe nella tabella
+// ****************funzione per inserire le bombe nella tabella & mostra tutte le bombe al momento del click sulla bomba
 function insertBomb(num) {
     const squareArray = document.getElementsByClassName("square");
-    const bombe = bombGenerator(num);
+    const bombe = bombGenerator(num); 
     for (let i = 0; i < bombe.length; i++) {
         const bigBomb = bombe[i];
         squareArray[bigBomb].addEventListener('click', function () {      
             for (let j = 0; j < bombe.length; j++) {
                 console.log(j);
-                squareArray[bombe[j]].classList.add("wrong");
+                squareArray[bombe[j]].classList.add("wrong"); 
+                squareArray[bombe[j]].innerHTML = `<i class="fas fa-fire fire"></i>`;          
             }
             squareArray[bigBomb].classList.add("wrongest");
         });
     }
 }
-
-
-
-
-
